529528e345152d70e1849d0dc4243495b3848d48
Atyan malik <atyanmalik8@gmail.com>
2025-11-07 07:12:58 -0800

    .

--- backend/Server.js
+++ backend/Server.js
@@ -9,39 +9,39 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
-// Allow frontend origin
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // your Vite frontend
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
 
-// store chat messages in memory (optional)
+
 let messages = [];
 
 io.on("connection", (socket) => {
-  console.log("🟢 New user connected:", socket.id);
+  console.log(" New user connected:", socket.id);
 
-  // send existing chat history to new user
+ 
   socket.emit("chatRoom", {
     user: "System",
     text: "Welcome to the chat 💬",
   });
 
-  // when message received from client
+ 
   socket.on("chatRoom", (msgData) => {
-    console.log("📩 Message received:", msgData);
+    console.log(" Message received:", msgData);
     messages.push(msgData);
 
-    // broadcast message to ALL users (including sender)
+  
     io.emit("chatRoom", msgData);
   });
 
   socket.on("disconnect", () => {
-    console.log("🔴 User disconnected:", socket.id);
+    console.log(" User disconnected:", socket.id);
   });
 });
 
 const PORT = 5000;
-server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(` Server running on port ${PORT}`));
